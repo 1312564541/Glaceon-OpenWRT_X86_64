a392c527a951f3573943ce183539359306cc7a05
Cysime Moflu <cysime@icloud.com>
2020-06-09 19:02:00 +0000

    Update for VSSR

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
